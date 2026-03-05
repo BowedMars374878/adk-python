119ce100cf01e2f4a29a49a9dc38d215ccb05aea
BowedMars2 <s-jonejar@bsd405.org>
2026-03-05 14:51:14 -0800

    Adds unit tests for example_util.convert_examples_to_text()

---
+++ tests/unittests/examples/test_example_util.py
@@ -0,0 +1,384 @@
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for example_util."""
+
+from google.adk.examples import example_util
+from google.adk.examples import example
+from google.genai import types
+import pytest
+
+
+def test_text_only_example_conversion():
+    """Tests converting a text-only Example object to a string for use in a system instruction."""
+    input_content = types.Content(
+        role="user",
+        parts=[types.Part(text="test_input")]
+    )
+    output_content = [types.Content(
+        role="model",
+        parts=[types.Part(text="test_output")]
+    )]
+    test_example = example.Example(
+        input=input_content,
+        output=output_content
+    )
+
+    expected_output = (
+        f"{example_util._EXAMPLES_INTRO}"
+        f"{example_util._EXAMPLE_START.format(1)}"
+        f"{example_util._USER_PREFIX}test_input\n"
+        f"{example_util._MODEL_PREFIX}test_output\n"
+        f"{example_util._EXAMPLE_END}"
+        f"{example_util._EXAMPLES_END}"
+    )
+
+    assert example_util.convert_examples_to_text(examples=[test_example], model=None) == expected_output
+
+
+def test_multi_part_text_example_conversion():
+    """Tests converting an Example object with multiple text Parts to a string for use in a system instruction."""
+    input_content = types.Content(
+        role="user",
+        parts=[types.Part(text="test_input")]
+    )
+    output_content = [types.Content(
+        role="model",
+        parts=[
+            types.Part(text="test_output_1"),
+            types.Part(text="test_output_2"),
+            types.Part(text="test_output_3"),
+        ]
+    )]
+    test_example = example.Example(
+        input=input_content,
+        output=output_content
+    )
+
+    expected_output = (
+        f"{example_util._EXAMPLES_INTRO}"
+        f"{example_util._EXAMPLE_START.format(1)}"
+        f"{example_util._USER_PREFIX}test_input\n"
+        f"{example_util._MODEL_PREFIX}test_output_1\ntest_output_2\ntest_output_3\n"
+        f"{example_util._EXAMPLE_END}"
+        f"{example_util._EXAMPLES_END}"
+    )
+
+    assert example_util.convert_examples_to_text(examples=[test_example], model=None) == expected_output
+
+
+def test_example_conversion_prefix_insertion():
+    """Tests if user and model prefixes are properly alternated when converting an Example object to text for use in a system instruction."""
+    input_content = types.Content(
+        role="user",
+        parts=[types.Part(text="test_input")]
+    )
+    output_content = [
+        types.Content(
+            role="model",
+            parts=[types.Part(text="test_output_1")]
+        ),
+        types.Content(
+            role="user",
+            parts=[types.Part(text="test_output_2")]
+        ),
+        types.Content(
+            role="model",
+            parts=[types.Part(text="test_output_3")]
+        )
+    ]
+    test_example = example.Example(
+        input=input_content,
+        output=output_content
+    )
+
+    expected_output = (
+        f"{example_util._EXAMPLES_INTRO}"
+        f"{example_util._EXAMPLE_START.format(1)}"
+        f"{example_util._USER_PREFIX}test_input\n"
+        f"{example_util._MODEL_PREFIX}test_output_1\n"
+        f"{example_util._USER_PREFIX}test_output_2\n"
+        f"{example_util._MODEL_PREFIX}test_output_3\n"
+        f"{example_util._EXAMPLE_END}"
+        f"{example_util._EXAMPLES_END}"
+    )
+
+    assert example_util.convert_examples_to_text(examples=[test_example], model=None) == expected_output
+
+
+def test_example_conversion_output_clumping():
+    """Tests whether user and model inputs are properly clumped when converting an Example object to text for use in a system instruction."""
+    input_content = types.Content(
+        role="user",
+        parts=[types.Part(text="test_input")]
+    )
+    output_content = [
+        types.Content(
+            role="model",
+            parts=[types.Part(text="test_output_1")]
+        ),
+        types.Content(
+            role="model",
+            parts=[types.Part(text="test_output_2")]
+        ),
+        types.Content(
+            role="user",
+            parts=[types.Part(text="test_output_3")]
+        ),
+        types.Content(
+            role="user",
+            parts=[types.Part(text="test_output_4")]
+        )
+    ]
+    test_example = example.Example(
+        input=input_content,
+        output=output_content
+    )
+
+    expected_output = (
+        f"{example_util._EXAMPLES_INTRO}"
+        f"{example_util._EXAMPLE_START.format(1)}"
+        f"{example_util._USER_PREFIX}test_input\n"
+        f"{example_util._MODEL_PREFIX}test_output_1\ntest_output_2\n"
+        f"{example_util._USER_PREFIX}test_output_3\ntest_output_4\n"
+        f"{example_util._EXAMPLE_END}"
+        f"{example_util._EXAMPLES_END}"
+    )
+
+    assert example_util.convert_examples_to_text(examples=[test_example], model=None) == expected_output
+
+
+def test_empty_examples_list_conversion():
+    """Tests Example conversion to text if the examples list is empty."""
+    expected_output = f"{example_util._EXAMPLES_INTRO}{example_util._EXAMPLES_END}"
+
+    assert example_util.convert_examples_to_text(examples=[], model=None) == expected_output
+
+
+@pytest.mark.parametrize(
+    "model",
+    [
+        "gemini-2.5-flash",
+        "llama3_vertex_agent",
+        None
+    ],
+)
+def test_example_conversion_with_function_call(model):
+    """Tests converting an Example object containing a function call to a string for use in a system instruction."""
+    test_function_call = types.FunctionCall(
+        name="test_function",
+        args={
+            "test_string_argument": "test_value",
+            "test_int_argument": 1
+        }
+    )
+    input_content = types.Content(
+        role="user",
+        parts=[types.Part(text="test_input")]
+    )
+    output_content = [types.Content(
+        role="model",
+        parts=[types.Part(function_call=test_function_call)]
+    )]
+    test_example = example.Example(
+        input=input_content,
+        output=output_content
+    )
+
+    gemini2 = model == "gemini-2.5-flash" or model == None
+    prefix = example_util._FUNCTION_PREFIX if gemini2 else example_util._FUNCTION_CALL_PREFIX
+    
+    expected_output = (
+        f"{example_util._EXAMPLES_INTRO}"
+        f"{example_util._EXAMPLE_START.format(1)}"
+        f"{example_util._USER_PREFIX}test_input\n"
+        f"{example_util._MODEL_PREFIX}{prefix}"
+        f"test_function(test_string_argument='test_value', test_int_argument=1)"
+        f"{example_util._FUNCTION_CALL_SUFFIX}"
+        f"{example_util._EXAMPLE_END}"
+        f"{example_util._EXAMPLES_END}"
+    )
+
+    assert example_util.convert_examples_to_text(examples=[test_example], model=model) == expected_output
+
+
+@pytest.mark.parametrize(
+    "model",
+    [
+        "gemini-2.5-flash",
+        "llama3_vertex_agent",
+        None
+    ],
+)
+def test_example_conversion_with_function_response(model):
+    """Tests converting an Example object containing a function response to a string for use in a system instruction."""
+    test_function_response = types.FunctionResponse(
+        name="test_function",
+        response={
+            "test_string_argument": "test_value",
+            "test_int_argument": 1
+        }
+    )
+    input_content = types.Content(
+        role="user",
+        parts=[types.Part(text="test_input")]
+    )
+    output_content = [types.Content(
+        role="model",
+        parts=[types.Part(function_response=test_function_response)]
+    )]
+    test_example = example.Example(
+        input=input_content,
+        output=output_content
+    )
+
+    gemini2 = model == "gemini-2.5-flash" or model == None
+    prefix = example_util._FUNCTION_PREFIX if gemini2 else example_util._FUNCTION_RESPONSE_PREFIX
+    
+    expected_output = (
+        f"{example_util._EXAMPLES_INTRO}"
+        f"{example_util._EXAMPLE_START.format(1)}"
+        f"{example_util._USER_PREFIX}test_input\n"
+        f"{example_util._MODEL_PREFIX}{prefix}"
+        f"{test_function_response.__dict__}"
+        f"{example_util._FUNCTION_RESPONSE_SUFFIX}"
+        f"{example_util._EXAMPLE_END}"
+        f"{example_util._EXAMPLES_END}"
+    )
+
+    assert example_util.convert_examples_to_text(examples=[test_example], model=model) == expected_output
+
+
+@pytest.mark.parametrize(
+    "model",
+    [
+        "gemini-2.5-flash",
+        "llama3_vertex_agent",
+        None
+    ],
+)
+def test_example_conversion_with_function_call_response(model):
+    """Tests converting an Example object containing a function call and response to a string for use in a system instruction."""
+    test_function_call = types.FunctionCall(
+        name="test_function",
+        args={
+            "test_string_argument": "test_value",
+            "test_int_argument": 1
+        }
+    )
+    test_function_response = types.FunctionResponse(
+        name="test_function",
+        response={
+            "test_string_argument": "test_value",
+            "test_int_argument": 1
+        }
+    )
+    input_content = types.Content(
+        role="user",
+        parts=[types.Part(text="test_input")]
+    )
+    output_content = [types.Content(
+        role="model",
+        parts=[
+            types.Part(function_call=test_function_call),
+            types.Part(function_response=test_function_response)
+        ]
+    )]
+    test_example = example.Example(
+        input=input_content,
+        output=output_content
+    )
+
+    gemini2 = model == "gemini-2.5-flash" or model == None
+    response_prefix = example_util._FUNCTION_PREFIX if gemini2 else example_util._FUNCTION_RESPONSE_PREFIX
+    call_prefix = example_util._FUNCTION_PREFIX if gemini2 else example_util._FUNCTION_CALL_PREFIX
+    
+    expected_output = (
+        f"{example_util._EXAMPLES_INTRO}"
+        f"{example_util._EXAMPLE_START.format(1)}"
+        f"{example_util._USER_PREFIX}test_input\n"
+        f"{example_util._MODEL_PREFIX}{call_prefix}"
+        f"test_function(test_string_argument='test_value', test_int_argument=1)"
+        f"{example_util._FUNCTION_CALL_SUFFIX}"
+        f"{response_prefix}"
+        f"{test_function_response.__dict__}"
+        f"{example_util._FUNCTION_RESPONSE_SUFFIX}"
+        f"{example_util._EXAMPLE_END}"
+        f"{example_util._EXAMPLES_END}"
+    )
+
+    assert example_util.convert_examples_to_text(examples=[test_example], model=model) == expected_output
+
+
+@pytest.mark.parametrize(
+    "model",
+    [
+        "gemini-2.5-flash",
+        "llama3_vertex_agent",
+        None
+    ],
+)
+def test_example_conversion_with_text_and_function_call_response(model):
+    """Tests converting an Example object containing text, a function call, and a function response to a string for use in a system instruction."""
+    test_function_call = types.FunctionCall(
+        name="test_function",
+        args={
+            "test_string_argument": "test_value",
+            "test_int_argument": 1
+        }
+    )
+    test_function_response = types.FunctionResponse(
+        name="test_function",
+        response={
+            "test_string_argument": "test_value",
+            "test_int_argument": 1
+        }
+    )
+    input_content = types.Content(
+        role="user",
+        parts=[types.Part(text="test_input")]
+    )
+    output_content = [types.Content(
+        role="model",
+        parts=[
+            types.Part(text="test_output"),
+            types.Part(function_call=test_function_call),
+            types.Part(function_response=test_function_response)
+        ]
+    )]
+    test_example = example.Example(
+        input=input_content,
+        output=output_content
+    )
+
+    gemini2 = model == "gemini-2.5-flash" or model == None
+    response_prefix = example_util._FUNCTION_PREFIX if gemini2 else example_util._FUNCTION_RESPONSE_PREFIX
+    call_prefix = example_util._FUNCTION_PREFIX if gemini2 else example_util._FUNCTION_CALL_PREFIX
+    
+    expected_output = (
+        f"{example_util._EXAMPLES_INTRO}"
+        f"{example_util._EXAMPLE_START.format(1)}"
+        f"{example_util._USER_PREFIX}test_input\n"
+        f"{example_util._MODEL_PREFIX}test_output\n"
+        f"{call_prefix}"
+        f"test_function(test_string_argument='test_value', test_int_argument=1)"
+        f"{example_util._FUNCTION_CALL_SUFFIX}"
+        f"{response_prefix}"
+        f"{test_function_response.__dict__}"
+        f"{example_util._FUNCTION_RESPONSE_SUFFIX}"
+        f"{example_util._EXAMPLE_END}"
+        f"{example_util._EXAMPLES_END}"
+    )
+
+    assert example_util.convert_examples_to_text(examples=[test_example], model=model) == expected_output
